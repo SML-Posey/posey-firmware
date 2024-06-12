cd68542c17634597c42c1b42a1a9cc186ddbe1ff
Anthony Wertz <awertz@pm.me>
2024-06-12 16:05:27 -0400

    Added some new board files, some reformatting.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "platform.hpp"
 
 #include <zephyr/sys/reboot.h>
+// #include "memfault/core/trace_event.h"
 
 #define LOG_MODULE_NAME posey_main
 LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 void die(const char * const message)
 {
+    // MEMFAULT_TRACE_EVENT_WITH_LOG(
+    //     system_death,
+    //     "Message: %s", message);
     LOG_ERR("%s", message);
     LOG_ERR("Waiting 5s then rebooting.");
     Clock::delay_msec(5000);
--- src/posey-platform/platform/sensors/imu_reset.c
+++ src/posey-platform/platform/sensors/imu_reset.c
@@ -1,16 +1,17 @@
 #include "imu_reset.h"
 
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(imu_reset);
 
-// static const struct gpio_dt_spec gpio_imu_reset = DT_GPIO_LABEL(DT_ALIAS(gpioimureset0), gpios);
-static const struct gpio_dt_spec gpio_imu_reset = GPIO_DT_SPEC_GET(DT_PATH(zephyr_user), imureset_gpios);
+// static const struct gpio_dt_spec gpio_imu_reset =
+// DT_GPIO_LABEL(DT_ALIAS(gpioimureset0), gpios);
+static const struct gpio_dt_spec gpio_imu_reset =
+    GPIO_DT_SPEC_GET(DT_PATH(zephyr_user), imureset_gpios);
 
-void imu_reset()
-{
+void imu_reset() {
     LOG_WRN("Will try to reset the IMU");
     if (!device_is_ready(gpio_imu_reset.port))
         LOG_ERR("Could not get IMU nRESET GPIO device");
